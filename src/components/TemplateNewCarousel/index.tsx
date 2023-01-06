5899691d8d3148c1a2f6432cb76d09c061acd7fb
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-01-06 20:01:50 -0300

    feat: responsive

--- src/components/TemplateNewCarousel/index.tsx
+++ src/components/TemplateNewCarousel/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import * as Chakra from "@chakra-ui/react";
-import { useBreakpointValue } from "@chakra-ui/react";
+import { Button, useBreakpointValue } from "@chakra-ui/react";
 
 interface ITemplateNewCarousel {
   header: ReactElement;
@@ -20,47 +20,35 @@ export const TemplateNewCarousel = ({
     xl: "xl",
   });
 
-  const [isCardVisible, setCardVisible] = useState(true);
+  const [isCardVisible, setCardVisible] = useState(false);
 
-  if (breakpoint === "sm")
-    return (
-      <Chakra.Grid
-        w="full"
-        h="100vh"
-        bg="gray.900"
-        color="white"
-        justifyItems="center"
-      >
-        <Chakra.Grid h="10vh">{header}</Chakra.Grid>
+  return (
+    <Chakra.Grid
+      w="full"
+      h="100vh"
+      bg="gray.900"
+      color="white"
+      justifyItems="center"
+      position="relative"
+    >
+      <Chakra.Grid h="10vh">{header}</Chakra.Grid>
 
-        <Chakra.Grid bg="gray.900" w="full" justifyItems="center">
-          <Chakra.Grid
-            h="full"
-            maxW="container.lg"
-            w="full"
-            gridTemplateColumns="1fr"
-          >
-            {isCardVisible ? (
-              <Chakra.Grid
-                w="full"
-                overflowY="auto"
-                h="90vh"
-                css={{
-                  "&::-webkit-scrollbar": {
-                    width: "4px",
-                  },
-                  "&::-webkit-scrollbar-track": {
-                    width: "6px",
-                  },
-                  "&::-webkit-scrollbar-thumb": {
-                    background: "#1A202C",
-                    borderRadius: "24px",
-                  },
-                }}
-              >
-                {rightSection}
-              </Chakra.Grid>
-            ) : (
+      <Button>Preview Carousel</Button>
+
+      <Chakra.Grid bg="gray.900" w="full" justifyItems="center">
+        <Chakra.Grid
+          h="full"
+          maxW="container.lg"
+          w="full"
+          gridTemplateColumns="1fr"
+        >
+          {breakpoint !== "sm" && (
+            <Chakra.Grid
+              h="full"
+              maxW="container.lg"
+              w="full"
+              gridTemplateColumns={["1fr", "1fr", "2fr 3fr", "1fr 2fr"]}
+            >
               <Chakra.Grid
                 w="full"
                 overflowY="auto"
@@ -80,11 +68,74 @@ export const TemplateNewCarousel = ({
               >
                 {leftSection}
               </Chakra.Grid>
-            )}
-          </Chakra.Grid>
+
+              <Chakra.Grid
+                w="full"
+                overflowY="auto"
+                h="90vh"
+                css={{
+                  "&::-webkit-scrollbar": {
+                    width: "4px",
+                  },
+                  "&::-webkit-scrollbar-track": {
+                    width: "6px",
+                  },
+                  "&::-webkit-scrollbar-thumb": {
+                    background: "#1A202C",
+                    borderRadius: "24px",
+                  },
+                }}
+              >
+                {rightSection}
+              </Chakra.Grid>
+            </Chakra.Grid>
+          )}
+          {breakpoint === "sm" && !isCardVisible && (
+            <Chakra.Grid
+              w="full"
+              overflowY="auto"
+              h="90vh"
+              css={{
+                "&::-webkit-scrollbar": {
+                  width: "4px",
+                },
+                "&::-webkit-scrollbar-track": {
+                  width: "6px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#1A202C",
+                  borderRadius: "24px",
+                },
+              }}
+            >
+              {leftSection}
+            </Chakra.Grid>
+          )}
+          {breakpoint === "sm" && isCardVisible && (
+            <Chakra.Grid
+              w="full"
+              overflowY="auto"
+              h="90vh"
+              css={{
+                "&::-webkit-scrollbar": {
+                  width: "4px",
+                },
+                "&::-webkit-scrollbar-track": {
+                  width: "6px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#1A202C",
+                  borderRadius: "24px",
+                },
+              }}
+            >
+              {rightSection}
+            </Chakra.Grid>
+          )}
         </Chakra.Grid>
       </Chakra.Grid>
-    );
+    </Chakra.Grid>
+  );
 
   return (
     <Chakra.Grid
