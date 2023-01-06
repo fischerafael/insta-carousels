06ac870bb66ab7abc87e62f6ea370264e5e48afd
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-01-06 19:54:56 -0300

    feat: what is visible

--- src/components/TemplateNewCarousel/index.tsx
+++ src/components/TemplateNewCarousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import * as Chakra from "@chakra-ui/react";
 import { useBreakpointValue } from "@chakra-ui/react";
 
@@ -20,7 +20,67 @@ export const TemplateNewCarousel = ({
     xl: "xl",
   });
 
-  console.log(breakpoint);
+  if (breakpoint === "sm")
+    return (
+      <Chakra.Grid
+        w="full"
+        h="100vh"
+        bg="gray.900"
+        color="white"
+        justifyItems="center"
+      >
+        <Chakra.Grid h="10vh">{header}</Chakra.Grid>
+
+        <Chakra.Grid bg="gray.900" w="full" justifyItems="center">
+          <Chakra.Grid
+            h="full"
+            maxW="container.lg"
+            w="full"
+            gridTemplateColumns="1fr"
+          >
+            <Chakra.Grid
+              w="full"
+              overflowY="auto"
+              h="90vh"
+              css={{
+                "&::-webkit-scrollbar": {
+                  width: "4px",
+                },
+                "&::-webkit-scrollbar-track": {
+                  width: "6px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#1A202C",
+                  borderRadius: "24px",
+                },
+              }}
+            >
+              {leftSection}
+            </Chakra.Grid>
+
+            <Chakra.Grid
+              w="full"
+              overflowY="auto"
+              h="90vh"
+              css={{
+                "&::-webkit-scrollbar": {
+                  width: "4px",
+                },
+                "&::-webkit-scrollbar-track": {
+                  width: "6px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  background: "#1A202C",
+                  borderRadius: "24px",
+                },
+              }}
+            >
+              {rightSection}
+            </Chakra.Grid>
+          </Chakra.Grid>
+        </Chakra.Grid>
+      </Chakra.Grid>
+    );
 
   return (
     <Chakra.Grid
@@ -37,9 +97,7 @@ export const TemplateNewCarousel = ({
           h="full"
           maxW="container.lg"
           w="full"
-          gridTemplateColumns={
-            breakpoint === "sm" ? "1fr" : ["1fr", "1fr", "2fr 3fr", "1fr 2fr"]
-          }
+          gridTemplateColumns={["1fr", "1fr", "2fr 3fr", "1fr 2fr"]}
         >
           <Chakra.Grid
             w="full"
